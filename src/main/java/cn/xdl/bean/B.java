b550b85a3e9c7e920ae9050aa116ff5e6e6cc619
javaziwen <2209719326@qq.com>
2019-11-15 18:08:31 +0800

    杀杀杀杀杀杀杀杀杀

--- src/main/java/cn/xdl/bean/B.java
+++ src/main/java/cn/xdl/bean/B.java
@@ -5,7 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		System.out.println("bbbbbbbbbbbb");
 
-		System.out.println("asdasdasdasdasdsadasdasdasdasda");
+		System.out.println("ahahhaha");
 
 	}
 
